e2ca7edb6844ac832a32168a236cdbead768fbe3
Студент KSI <student@ksi.esstu.ru>
2022-10-13 17:34:08 +0800

    практика 2

--- controllers/default.js
+++ controllers/default.js
@@ -1,10 +1,18 @@
 exports.install = function() {
-    ROUTE('+GET /*'                     );
-    ROUTE('GET /login',      view_login);   
+    ROUTE('+GET /*'                             );
+    ROUTE('GET  /login',              view_login);   
+    ROUTE('GET  /logout',                 logout); 
 }
 
 function view_login() {
     var self = this;   
     self.sm = F.sitemap('auth');                      
     self.view('page/login');
+}
+
+function logout() {
+    var self = this;        
+    MAIN.session.remove(self.sessionid);    
+    self.cookie(CONF.cookie, '', '-1 day');                
+    self.redirect('/');	
 }
